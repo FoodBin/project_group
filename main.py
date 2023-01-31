e747c83c64d2f8c7c642f0fb2e4824130166ece2
Gideon_Wong <S10221064@connect.np.edu.sg>
2023-02-01 05:46:45 +0800

    Tested Overheads in the main

---
+++ main.py
@@ -0,0 +1,15 @@
+import overheads
+
+
+def main():
+    """
+    - This function will be the one that combines all modular programs
+    - This function will import cash_on_hand.py, overheads.py, and profit_loss.py
+    - This function does not require any parameters
+    """
+    
+    overheads.overheads_function()
+
+
+
+
