a439ad706d13f8a33ec1fcfc437e41f1db56366c
Gideon_Wong <S10221064@connect.np.edu.sg>
2023-02-02 00:52:09 +0800

    Updated

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -11,18 +11,18 @@ with COH_fp.open(mode = "r", encoding = "UTF-8", newline = "") as file:
     next(reader) #Skip the header
 
     #Create an empty list that will be used to store calculated deficits if detected
-    Cash_on_Hand = []
+    Day_Coh = []
     Deficit = []
 
-    #Create a loop
-    for day, coh in reader:
-
-        #Bring over the values from the CSV files into the, Cash_on_Hand, empty list
-        Cash_on_Hand.append([day, coh])
-        Cash_on_Hand.append(float(coh))
-        #Create a condition that will calculate cash deficit if found.
-        if coh[0] > coh[1]:
-            Deficit.append[coh[0] - coh[1]]
+    #Create a "for" loop 
+    for row in reader:
+        Day_Coh.append([float(row[0]),float(row[1])])
+    
+    for difference in Day_Coh:
+        Difference = Day_Coh[1][1] - Day_Coh[0][1]
+        Difference += 1
 
 
-print (Cash_on_Hand)
+
+
+print (Difference)
--- overheads.py
+++ overheads.py
@@ -20,22 +20,18 @@ def overheads_function():
 
     #Create a lists to sort the values
         List = []
-        Highest = []
 
-    #Create a loop to append the new empty lists while sorting the numbers in descending order
+    #Create a loop to append the new empty lists
         for row in reader:
             List.append([float(row[1]),row[0]]) #Change numerical values to a float
-            List.sort(reverse=True)
- 
-    #Seperate the highest overhead from the rest into the "Highest" empty list
-        Highest.append(List[0])
+            List.sort(reverse=True) #Sort the numbers in descending order
 
     #Create a file path which the text and numbers will be entered into
         Overheads_Filepath = Path.cwd()/"Summary_report.txt"
         with Overheads_Filepath.open(mode = "a", encoding= "UTF-8") as file:
 
     #Use "f-string" to write out the highest overhead with text
-            file.write(f"[HIGHEST OVERHEADS] {Highest[0][1]}: {Highest[0][0]}%")
+            file.write(f"[HIGHEST OVERHEADS] {List[0][1]}: {List[0][0]}%")
 
 #Execute the function
 overheads_function()
